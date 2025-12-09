3fc5ce32234a918005401cc783b5af596f13ed53
sherlin@97 <sherlinmary33@gmail.com>
2025-12-09 14:10:51 +0530

    added path type

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -9,12 +10,23 @@ def main():
         if command == 'exit':
             return False
         elif command.startswith('type'):
-            builtin_commands = ['echo', 'exit', 'type']
+            builtin_commands = ['echo', 'exit']
             typed_input = command.split('type ')[1]
             if typed_input in builtin_commands:
                 print(f"{typed_input} is a shell builtin")
-            else:
-                print(f"{typed_input}: not found")
+                main()
+
+            path_dirs = os.getenv("PATH","").split(os.pathsep)
+            for directory in path_dirs:
+                if not os.path.isdir(directory):
+                    continue
+                full_path = os.path.join(directory, typed_input)
+                if os.path.isfile(full_path):
+                    # Check execute permission
+                    if os.access(full_path, os.X_OK):
+                        print(f"{typed_input} is {full_path}")
+                        main()
+            print(f"{typed_input}: not found")
 
         elif command.startswith('echo'):
             messsage = command.split('echo ')[1]
